128814b7e87dbd69c76a74729f709ab47e8c0c0e
David Briscoe <idbrii@gmail.com>
2021-12-12 00:24:40 -0800

    Create test projects on demand
    Instead of cluttering the project with these tiny projects, create
    them while running tests. Makes it easier to eyeball the test and see
    what we're testing.

--- tests/parse_love_conf.py
+++ tests/parse_love_conf.py
@@ -3,18 +3,85 @@ from pathlib import Path
 
 from makelove.config import get_config
 
+toml = """
+default_targets = [
+    "win32",
+]
+build_directory = "build"
 
-def load_project(sub_path):
-    proj_path = Path(__file__).parent / sub_path
-    os.chdir(proj_path.parent)
-    return get_config(proj_path)
+love_files = [
+    "::git-ls-tree::",
+    "-*/.*",
+]
+
+[lovejs]
+"""
+
+test_project = Path(__file__).parent / "test_project"
+test_project.mkdir(exist_ok=True)
+os.chdir(test_project)
+makelove_path = test_project / "makelove.toml"
+with makelove_path.open("w") as f:
+    f.write(toml)
 
 
-cfg = load_project("test_projects/duplicates/makelove.toml")
-assert cfg["name"] not in ["normalname", "debugname"]
-assert cfg["love_version"] not in ["11.0", "11.1"]
+def load_project(conf_name, conf_text):
+    conf = test_project / conf_name
+    with conf.open("w") as f:
+        f.write(conf_text)
+    cfg = get_config(makelove_path)
+    conf.unlink()
+    return cfg
 
-cfg = load_project("test_projects/guess_all/makelove.toml")
+
+print("Test All guessables")
+cfg = load_project(
+    "conf.lua",
+    """function love.conf(t)
+    t.identity     = 'namefromconf'
+    t.version      = '11.1'
+    t.window.title = 'Title of Window'
+end""",
+)
 assert cfg["name"] == "namefromconf"
 assert cfg["love_version"] == "11.1"
 assert cfg["lovejs"]["title"] == "Title of Window"
+
+
+print("Test Duplicates")
+cfg = load_project(
+    "conf.lua",
+    """
+function love.conf(t)
+    if true then
+        t.identity     = 'normalname'
+        t.version      = '11.0'
+        t.window.title = 'Title of Window'
+    else
+        t.identity     = 'debugname'
+        t.version      = '11.1'
+        t.window.title = 'Debug Window'
+    end
+end
+""",
+)
+assert cfg["name"] not in ["normalname", "debugname"]
+assert cfg["love_version"] not in ["11.0", "11.1"]
+
+
+print("Test Comments")
+cfg = load_project(
+    "conf.lua",
+    """function love.conf(t)
+    --t.identity     = 'namefromconf'
+    -- t.version      = '11.1'
+    -- OLD title: t.window.title = 'Title of Window'
+end""",
+)
+assert cfg["name"] != "namefromconf"
+assert cfg["love_version"] != "11.1"
+assert "title" not in cfg["lovejs"]
+
+makelove_path.unlink()
+os.chdir(test_project.parent)
+test_project.rmdir()
--- tests/test_projects/guess_all/conf.lua
+++
@@ -1,5 +0,0 @@
-function love.conf(t)
-    t.identity     = 'namefromconf'
-    t.version      = '11.1'
-    t.window.title = 'Title of Window'
-end
